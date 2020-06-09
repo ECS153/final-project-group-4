cfcbd11b0c0489794e9d33b528278d87e4746738
andrewsamuelson <48627189+andrewsamuelson@users.noreply.github.com>
2020-06-09 12:22:51 -0700

    Add files via upload
    Android App built in Android studio with the intent to take in c++ on the backend

---
+++ app/src/main/java/com/example/onepadapp/MainActivity.java
@@ -0,0 +1,54 @@
+
+//first app I have ever worked on
+//https://developer.android.com/training/basics/firstapp
+package com.example.onepadapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    public static final String EXTRA_MESSAGE = "com.example.onepadapp.MESSAGE";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        //TextView tv = findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
+    }
+
+    /**
+     * Called when the user taps the Send button
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, showMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+
+
+    }
